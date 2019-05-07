4d4efc3893abb55405fa06c14ad925a8b24709d8
houssamb <houssam.b@hotmail.com>
2019-05-07 14:39:21 -0400

    starting on table 4

--- load_data.js
+++ load_data.js
@@ -23,6 +23,27 @@ function spendingPerType(data) {
 }
 
 
+function solicitationData(data) {
+  var solicitationGroup =  _.chain(data).groupBy('solicitation_procedure_code').value();
+  let result = [];
+  for (var index in solicitationGroup) {
+    var contracts_count = _.reduce(_.pluck(solicitationGroup[index], 'contracts_count'), function(memo, num) { return memo + parseInt(num) },0);
+    var original_value = _.reduce(_.pluck(solicitationGroup[index], 'original_value'), function(memo, num){ return memo + parseInt(num); }, 0);
+    var percent_total = 100 * (contracts_count / _.reduce(_.pluck(data, 'contracts_count'), function(memo, num){ return memo + parseInt(num); }, 0));
+    var percent_total_value = 100 * (original_value / _.reduce(_.pluck(data, 'original_value'), function(memo, num){ return memo + parseInt(num); }, 0));
+    result.push({
+      "Commodity type": index,
+      "Number": contracts_count,
+      "Percent of total number of contracts": formatPercent(percent_total),
+      "Value" : formatDollar(original_value),
+      "Percent of total value": formatPercent(percent_total_value)
+    });
+  }
+  console.log(result);
+  return result;
+}
+
+
 
 function consumeData(error, under25k_data, over25k_data) {
 
@@ -30,30 +51,34 @@ function consumeData(error, under25k_data, over25k_data) {
       console.log("Error on data load");
   }
 
-
   // Table 1 = tables 2 + 3
+  let table1_output = spendingPerType(_.union(under25k_data, over25k_data));
+  populateTable(table1_output, 'table1');
   function drawChart1(){
-    let output = spendingPerType(_.union(under25k_data, over25k_data));
-    console.log(JSON.stringify(output));
-    populateTable(output, 'table1');
-    drawBarGraph('chart1', output, "Commodity type", "Number");
+    drawBarGraph('chart1', table1_output, "Commodity type", "Number");
   }
 
   // Table 2
+  let table2_output = spendingPerType(under25k_data);
+  populateTable(table2_output, 'table2');
   function drawChart2(){
-    let under25k_output = spendingPerType(under25k_data);
-    console.log(JSON.stringify(under25k_output));
-    populateTable(under25k_output, 'table2');
-    drawBarGraph('chart2', under25k_output, "Commodity type", "Number");
+    drawBarGraph('chart2', table2_output, "Commodity type", "Number");
   }
 
   // Table 3
+  let table3_output = spendingPerType(over25k_data);
+  populateTable(table3_output, 'table3');
   function drawChart3(){
-    let over25k_output = spendingPerType(over25k_data);
-    console.log(JSON.stringify(over25k_output));
-    populateTable(over25k_output, 'table3');
-    drawBarGraph('chart3', over25k_output, "Commodity type", "Number");
+    drawBarGraph('chart3', table3_output, "Commodity type", "Number");
   }
+
+
+  // Table 4
+  let table4_output = solicitationData(over25k_data);
+
+
+
+
 
 
 // Animate Chart drawing using Materialize
@@ -63,8 +88,6 @@ function consumeData(error, under25k_data, over25k_data) {
     {selector: '#chart3', offset:150, callback: drawChart3}
   ];
   Materialize.scrollFire(options);
-
-
 
 }
 
@@ -76,29 +99,3 @@ d3.queue()
   .defer(d3.csv, 'under-25k.csv')
   .defer(d3.csv, 'over-25k.csv')
   .await(consumeData); //only function name is needed
-
-
-
-
-
-// function consumeData(data) {
-//   var periods = _.uniq(_.map(_.pluck(data, 'reporting_period'), function(period) {
-//     return period.substring(0, period.length-3);
-//   }));
-//
-//   var mappedData = _.map(periods, function(period){
-//      var length = _.reject(data, function(el){
-//            return (el.reporting_period.indexOf(period) < 0);
-//      }).length;
-//      return {period: period, count: length};
-//    });
-//
-//    console.log(JSON.stringify(mappedData));
-//    drawBarGraph('myChart', mappedData);
-//    populateTable(mappedData);
-//  }
-//
-//
-// d3.csv("contracts-nil.csv", function(data) {
-//   consumeData(data);
-// });
